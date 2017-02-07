bef3f755f1b552532901695814ab8ab970a1d845
Erik Swanson <erik@automatedinsights.com>
2017-02-07 11:23:31 -0500

    phone

--- regex_hw.rb
+++ regex_hw.rb
@@ -23,3 +23,15 @@ class EmailFinder
     data.flatten.select {|s| s.match(^([\w\d_\-\.]+)@([\w\d_\-\.]+)\.([\w]{2,5})$)}
   end
 end
+
+class PhoneFinder
+  attr_reader :data
+
+  def initialize(filename)
+    @data = CSV.read(filename)
+  end
+
+  def get_numbers
+    data.flatten.select {|s| s.match((^(\(([\d]{3})\)\s?|[^0]\d{2}(-|\s|\.))\d{3}(-|\s|\.)\d{4}$)|(^[^0]\d{9}$))}
+  end
+end
