e6597a93d2603e567f5ac94d5dbcbdfa0bd3dd3d
Erik Swanson <erik@automatedinsights.com>
2017-02-07 11:22:53 -0500

    email

--- regex_hw.rb
+++ regex_hw.rb
@@ -11,3 +11,15 @@ class DateFinder
     data.flatten.select {|s| s.match(^([1][0-2]|[0]\d|\d)[\/-]([3][01]|[0-2]\d|\d)[\/-](\d{2}|\d{4})$)}
   end
 end
+
+class EmailFinder
+  attr_reader :data
+
+  def initialize(filename)
+    @data = CSV.read(filename)
+  end
+
+  def get_numbers
+    data.flatten.select {|s| s.match(^([\w\d_\-\.]+)@([\w\d_\-\.]+)\.([\w]{2,5})$)}
+  end
+end
